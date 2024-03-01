d6b9fc5c013c09f466f59b964de1d519b49cab90
CATA Sistemas <sistemascatatsas@gmail.com>
2024-03-01 16:22:27 -0300

    se vuele para atras

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 // api.js
 import axios from 'axios';
 
-const apiUrl = '/api/proxy';
+const apiUrl = 'http://datos.energia.gob.ar/api/3/action/datastore_search';
 
 export const obtenerDatos = async (parametros) => {
   try {
-    const response = await axios.get(`${apiUrl}`);
+    const response = await axios.get(`${apiUrl}`, { params: parametros });
     return response.data;
   } catch (error) {
     console.error('Error al obtener datos:', error);
